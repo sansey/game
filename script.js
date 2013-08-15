8c3cd94a63cc2284c420ec11c90321ea37df1692
furball <furball@bigmir.net>
2013-08-15 07:41:15 +0300

    output added

--- script.js
+++ script.js
@@ -1,6 +1,22 @@
 (function () {
 	"use strict"
 
+	var lineStopped = document.createEvent("UIEvents"),
+		spinningStopped = document.createEvent("UIEvents"),
+		spinEls = document.querySelectorAll("[data-el-order]"),
+		betWrapper = document.getElementsByClassName("bet-lines")[0],
+		brokeAlertWrapper = document.querySelector(".wrapper .broke-alert"),
+		lootCounter = document.querySelector(".loot output"),
+		spinObjs = [],
+		startButton = document.getElementsByClassName("go")[0],
+		spinnerManager,
+		opt = {
+			PERIOD: 5,
+			DELTA: 4
+		};
+	lineStopped.initEvent("lineStopped", true, false);
+	spinningStopped.initEvent("spinningStopped", true, false);
+
 	function Spinner(el, options) {
 		var interval,
 			opt = {
@@ -18,11 +34,23 @@
 			that = this,
 			isIncreasingSpeed = true,
 			bgPosPrefix = "0 ",
-			bgPosPostfix = "px";
+			bgPosPostfix = "px",
+			finalNumbers,
+			WINNINGNUMBERSLENGTH = 3;
 
 		function reset(){
 			isIncreasingSpeed = true;
 			accelerationBase = 0;
+		}
+
+		function setFinalNumbers(){
+			var dy = opt.curBgPostion / elHeight,
+				secondLapNumbers = dy + WINNINGNUMBERSLENGTH - opt.order.length;
+			if(secondLapNumbers > 0){
+				finalNumbers = opt.order.slice(dy, opt.order.length).concat(opt.order.slice(0, secondLapNumbers));
+			}else{
+				finalNumbers = opt.order.slice(dy, dy + WINNINGNUMBERSLENGTH);
+			}
 		}
 
 		function getAcceleration(isIncreasing) {
@@ -33,11 +61,18 @@
 			return Math.floor(accelerationBase);
 		}
 
+		function stop() {
+			clearInterval(interval);
+			interval = null;
+		}
+
 		function moveUntilEdge(){
 			var interval = setInterval(function(){
 				moveIt(0);
 				if(opt.curBgPostion % elHeight === 0){
+					setFinalNumbers();
 					clearInterval(interval);
+					el.dispatchEvent(lineStopped);
 				}
 			}, opt.PERIOD);
 		}
@@ -56,7 +91,7 @@
 					var acceleration = getAcceleration(isIncreasingSpeed);
 					if (opt.DELTA + acceleration < speedMinimum) {
 						moveUntilEdge();
-						that.stop();
+						stop();
 						return;
 					}
 					moveIt(acceleration);
@@ -68,10 +103,7 @@
 			opt[i] = options[i];
 		}
 		elHeight = Math.floor(opt.height / opt.order.length);
-		this.stop = function () {
-			clearInterval(interval);
-			interval = null;
-		};
+		this.el = el;
 		this.stopping = function () {
 			isIncreasingSpeed = false;
 		};
@@ -79,29 +111,114 @@
 			reset();
 			spin();
 		};
+		this.getFinalNumbers = function(){
+			return finalNumbers;
+		}
 	}
 
-	function SpinnerManager(objArray) {
-		var that = this;
-		["start", "stopping"].forEach(function (command) {
-			that[command] = function () {
-				objArray.forEach(function (obj) {
-					obj[command]();
-				});
+	function SpinnerManager(linesArray, button, winLinesWrapper, lootContainer, brokeAlertWrapper) {
+		var ROLLTIME = 1000,
+			linesNumber = linesArray.length,
+			stoppedLinesNumber = 0,
+			pressedButtonClass = "go pressed",
+			buttonClass = "go",
+			isAbleSpin = true,
+			startCommand = "start",
+			stopCommand = "stopping",
+			winLinesDefaultClasses,
+			coins;
+
+		function Coins(){
+			var WINCOINNUMBER = 3,
+				DEFAULTCOINSNUMBER = 20,
+				brokeAlertClassName = "broke-alert",
+				brokeAlertShowClassName = "shown",
+				coins;
+			function checkBroke(){
+				if(coins < 1){
+					brokeAlertWrapper.className += brokeAlertShowClassName;
+				}
+			}
+			this.win = function(){
+				coins += WINCOINNUMBER
 			};
+			this.loose = function(){
+				coins--;
+				checkBroke();
+			};
+			this.reset = function(){
+				coins = DEFAULTCOINSNUMBER;
+				brokeAlertWrapper.className = brokeAlertClassName;
+			};
+			this.getAmount = function(){
+				return coins;
+			};
+			this.reset();
+		}
+
+		function countLoot(){
+			var finalNumbersArray = [], i,
+				linesLength = linesArray.length,
+				classPrefix = "nth-";
+			linesArray.forEach(function(line){
+				finalNumbersArray.push(line.getFinalNumbers());
+			});
+			winLinesDefaultClasses = winLinesWrapper.className;
+			for(i = 0; i < linesLength; i++){//TODO: remove magic numbers
+				if(finalNumbersArray[0][i] === finalNumbersArray[1][i] === finalNumbersArray[2][i]){
+					winLinesWrapper.className += classPrefix + i;
+					coins.win();
+				}
+			}
+			if(finalNumbersArray[0][0] === finalNumbersArray[1][1] === finalNumbersArray[2][2]){
+				winLinesWrapper.className += classPrefix + 3;
+				coins.win();
+			}
+			if(finalNumbersArray[0][2] === finalNumbersArray[1][1] === finalNumbersArray[2][0]){
+				winLinesWrapper.className += classPrefix + 4;
+				coins.win();
+			}
+			coins.loose();
+			lootContainer.innerHTML = coins.getAmount();
+		}
+		coins = new Coins();
+		lootContainer.innerHTML = coins.getAmount();
+		button.addEventListener("click", function () {
+			button.className = pressedButtonClass;
+			spinnerManager.start();
+			setTimeout(function () {
+				spinnerManager.stopping();
+			}, ROLLTIME);
+		});
+		this[stopCommand] = function () {
+			linesArray.forEach(function (obj) {
+				obj[stopCommand]();
+			});
+		};
+		this[startCommand] = function () {
+			if(isAbleSpin && coins.getAmount() > 0){
+				isAbleSpin = false;
+				linesArray.forEach(function (obj) {
+					obj[startCommand]();
+				});
+			}
+		};
+		linesArray.forEach(function(obj){
+			obj.el.addEventListener("lineStopped", function(){
+				stoppedLinesNumber++;
+				if(stoppedLinesNumber === linesNumber){
+					stoppedLinesNumber = 0;
+					document.dispatchEvent(spinningStopped);
+				}
+			});
+		});
+		document.addEventListener("spinningStopped", function(){
+			countLoot();
+			button.className = buttonClass;
+			isAbleSpin = true;
 		});
 	}
 
-	var spinEls = document.querySelectorAll(".wrapper div"),
-		spinObjs = [],
-		startButton = document.getElementsByClassName("go")[0],
-		spinnerManager,
-		pressedButtonClass = "go pressed",
-		buttonClass = "go",
-		opt = {
-			PERIOD: 5,
-			DELTA: 4
-		};
 	Array.prototype.forEach.apply(spinEls, [function (el, index) {
 		var i;
 		for (i in opt) {
@@ -111,13 +228,10 @@
 		opt["height"] = parseInt(el.getAttribute("data-el-height"));
 		spinObjs.push(new Spinner(el, opt));
 	}]);
-	spinnerManager = new SpinnerManager(spinObjs);
-	startButton.addEventListener("click", function () {
-		startButton.className = pressedButtonClass;
-		spinnerManager.start();
-		setTimeout(function () {
-			spinnerManager.stopping();
-			startButton.className = buttonClass;
-		}, 5000);
+	["mousedown","mouseup","click"].forEach(function(eventName){
+		brokeAlertWrapper.addEventListener(eventName, function(e){
+			e.stopPropagation();
+		});
 	});
+	spinnerManager = new SpinnerManager(spinObjs, startButton, betWrapper, lootCounter, brokeAlertWrapper);
 })();
